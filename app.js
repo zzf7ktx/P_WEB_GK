ba51a0a6f439d62d6378e66aec782a2812b857ec
zzf7ktx <zzf7ktx@gmail.com>
2019-11-02 23:12:36 +0700

    Add slide most popular, collection and add route login

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var hbs = require('express-handlebars')
+var loginRouter = require('./routes/login');
+var hbs = require('express-handlebars');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
