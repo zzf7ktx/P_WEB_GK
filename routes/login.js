176ed61f22f05aa40c62120de2c3fb0146685f93
zzf7ktx <zzf7ktx@gmail.com>
2019-11-03 19:09:37 +0700

    update login and signup page

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
